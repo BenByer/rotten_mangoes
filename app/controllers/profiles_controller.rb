de6e36cb04f2cf3ced319bca309973d638c5d513
BenByer <benbyer@shaw.ca>
2016-04-28 12:46:37 -0700

    fixed email issues

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,20 @@
+class ProfilesController < ApplicationController
+  def show
+    # Apr 28 lesson stuff
+    # if params[:id]
+    #   @user = User.find(params[:id])
+    # elsif current_user
+    #   @user = current_user
+    # else
+    #   raise ActiveRecord::RecordNotFound
+    # end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+end
